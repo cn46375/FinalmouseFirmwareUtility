a6aa77be87cf0a0a5f99e86693b270028ff3b617
crush <crush@CN46375>
2022-04-22 00:47:34 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace FinalmouseFirmwareUtility
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 401);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Status: No devices found in DFU Mode";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Find DFU Device";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(7, 577);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Update Firmware";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(415, 75);
+            this.label2.TabIndex = 5;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(7, 325);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(215, 21);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Step 3: Check for DFU devices";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(7, 474);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(220, 21);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Step 4: Flash Firmware Update";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(7, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 21);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Step 1: Put Starlight-12 in DFU mode";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(7, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(304, 21);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Step 2: Close background monitoring apps";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(308, 60);
+            this.label5.TabIndex = 16;
+            this.label5.Text = resources.GetString("label5.Text");
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 535);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(216, 23);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 508);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 15);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Select firmware to flash:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FinalmouseFirmwareUtility.Properties.Resources.FinalmouseZeus;
+            this.ClientSize = new System.Drawing.Size(447, 612);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Finalmouse Firmware Update Utility";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+        private Label label9;
+        private Label label10;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private ComboBox comboBox1;
+        private Label label6;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalmouseFirmwareUtility
+{
+    public partial class Form1 : Form
+    {
+        List<string> currentPorts = SerialPort.GetPortNames().ToList();
+        List<string> dfuPorts = new List<string>();
+        Dictionary<string, string> firmwareDictionary = GetFirmwareDictionary();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var text = "Welcome to the Finalmouse Firmware Update Utility. " +
+                "If your Starlight-12 is already in DFU mode before running this utility, " +
+                "please disconnect your mouse from the included micro-USB cable and restart this program again. " +
+                "Otherwise, proceed to begin the firmware update.";
+
+            var title = "Finalmouse Firmware Update Utility";
+
+            MessageBox.Show(text, title);
+
+            foreach (var firmware in firmwareDictionary)
+            {
+                comboBox1.Items.Add(firmware.Value);
+            }
+
+            comboBox1.SelectedIndex = 3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var updatedPorts = SerialPort.GetPortNames().ToList();
+
+            dfuPorts = updatedPorts.Where(x => !currentPorts.Any(y => x == y)).ToList();
+
+            if (dfuPorts.Count() > 0)
+            {
+                var msgText = "Found device in DFU mode. Click on the Update button below to begin the firmware update.";
+                var msgTitle = "DFU Device Found";
+
+                MessageBox.Show(msgText, msgTitle);
+
+                label1.Text = $"Status: Current device in DFU Mode: {dfuPorts.First()}.\nClick on \"Update Firmware\" to begin the update.";
+
+                button2.Enabled = true;
+            }
+            else
+            {
+                label1.Text = $"Status: No devices found in DFU Mode.";
+
+                var msgText = "No devices found in DFU mode. Please make sure you have done the following:\n\n" +
+                                "1. Unplug the wireless receiver from the micro-USB cable and turn the mouse off.\n" +
+                                "2. Hold down M3 (middle-click) and the DPI button.\n" +
+                                "3. Plug in your Starlight-12 with the included micro-USB cable while performing (2).";
+                var msgTitle = "No Devices Found";
+
+                MessageBox.Show(msgText, msgTitle);
+
+                button2.Enabled=false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var selectedFw = firmwareDictionary.ElementAt(comboBox1.SelectedIndex).Key;
+
+            var startInfo = new System.Diagnostics.ProcessStartInfo()
+            {
+                WorkingDirectory = Directory.GetCurrentDirectory() + "\\Resources",
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal,
+                FileName = "cmd.exe",
+                Arguments = $"/c nrfutil dfu usb-serial -pkg {selectedFw} -p {dfuPorts.First()} -b 115200 & PAUSE",
+                RedirectStandardInput = false,
+                UseShellExecute = false
+            };
+
+            var proc = new System.Diagnostics.Process();
+            proc.StartInfo = startInfo;
+            proc.Start();
+
+            button2.Enabled = false;
+
+            label1.Text = $"Status: No devices found in DFU Mode.";
+        }
+
+        private static Dictionary<string, string> GetFirmwareDictionary()
+        {
+            var firmwareDictionary = new Dictionary<string, string>();
+
+            firmwareDictionary.Add("fm6_dfu_package_0.5.80.zip", "0.5.80");
+            firmwareDictionary.Add("fm6_dfu_package_1.2.0_200ms.zip", "1.2.0 - 200ms scroll wheel delay");
+            firmwareDictionary.Add("fm6_dfu_package_1.2.0_iamadumbass_debounce.zip", "1.2.0 - 1.5ms sym defer");
+            firmwareDictionary.Add("fm6_dfu_package_1.2.5.zip", "1.2.5 - Poseidon/Starlight Pro");
+
+            return firmwareDictionary;
+        }
+    }
+}
